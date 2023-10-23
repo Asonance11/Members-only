324f876b9a1a243fed803b1dc3d8051051fb6eeb
Oluwadabira <adigunsamuelaso@gmail.com>
2023-10-23 22:10:59 +0100

    Setting up the server and adding connection to DB

--- index.ts
+++ index.ts
@@ -1,9 +1,52 @@
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import createError from 'http-errors';
+import mongoose from 'mongoose';
+import logger from 'morgan';
+import path from 'path';
 
 dotenv.config();
 
 const app: Application = express();
+
+const mongoUrl = process.env.MONGODB_URL || '';
+
+async function main() {
+	try {
+		await mongoose.connect(mongoUrl);
+		console.log('Connected to MongoDB		');
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+	next(createError(404));
+});
+
+// error handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
