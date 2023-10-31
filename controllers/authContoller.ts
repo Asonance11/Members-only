0e5215ce3220ca595533c2df496c1f5fbadbac08
Oluwadabira <adigunsamuelaso@gmail.com>
2023-10-31 11:38:15 +0100

    editing signup_post route

--- controllers/authContoller.ts
+++ controllers/authContoller.ts
@@ -1,9 +1,23 @@
-import { NextFunction, Request, Response } from 'express';
-import asyncHandler from 'express-async-handler';
-export const user_create_get = asyncHandler(
-	async (req: Request, res: Response, next: NextFunction) => {
-		res.render('signup', {
-			title: 'Sign up',
-		});
-	}
+import { NextFunction, Request, Response } from "express";
+import asyncHandler from "express-async-handler";
+import { body, validationResult } from "express-validator";
+export const signup_get = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.render("signup", {
+      title: "Sign up",
+    });
+  }
 );
+
+export const signup_post = [
+  body("username")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Username must consist of at least on character"),
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    res.render("signup", {
+      title: "Sign up",
+    });
+  }),
+];
