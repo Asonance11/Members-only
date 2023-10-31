2a79a89ce6bace142ae76b2638d33d07fc293331
Oluwadabira <adigunsamuelaso@gmail.com>
2023-10-31 11:37:45 +0100

    change of route name

--- routes/signup.ts
+++ routes/signup.ts
@@ -1,8 +1,8 @@
 import express from 'express';
-import { user_create_get } from '../controllers/authContoller';
+import { signup_get } from '../controllers/authContoller';
 
 const router = express.Router();
 
-router.get('/signup', user_create_get);
+router.get('/signup', signup_get);
 
 export default router;
