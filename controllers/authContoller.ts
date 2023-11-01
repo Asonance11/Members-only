37ceabd3ab541f56af7d96b253f4bae43956b058
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-01 05:13:23 +0100

    addition of signup post

--- controllers/authContoller.ts
+++ controllers/authContoller.ts
@@ -1,23 +1,62 @@
-import { NextFunction, Request, Response } from "express";
-import asyncHandler from "express-async-handler";
-import { body, validationResult } from "express-validator";
-export const signup_get = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    res.render("signup", {
-      title: "Sign up",
-    });
-  }
-);
+import bcrypt from 'bcryptjs';
+import { NextFunction, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import User from '../models/user';
+export const signup_get = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	res.render('signup', {
+		title: 'Sign up',
+	});
+};
 
 export const signup_post = [
-  body("username")
-    .trim()
-    .isLength({ min: 1 })
-    .withMessage("Username must consist of at least on character"),
-  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    res.render("signup", {
-      title: "Sign up",
-    });
-  }),
+	body('username')
+		.trim()
+		.isLength({ min: 1 })
+		.withMessage('Username must consist of at least on character'),
+	body('password')
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage('Password must be more than one character'),
+	async (req: Request, res: Response, next: NextFunction) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			res.render('signup', {
+				title: 'Sign up',
+				errors: errors.array(),
+			});
+			return;
+		}
+		try {
+			const userExists = await User.findOne({ username: req.body.username });
+
+			if (userExists) {
+				res.render('signup', {
+					title: 'Sign up',
+					errors: [{ msg: 'Username already exists' }],
+				});
+				return;
+			}
+
+			const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+			const user = new User({
+				username: req.body.username,
+				password: hashedPassword,
+				member: false,
+				admin: false,
+			});
+
+			await user.save();
+
+			res.redirect('/');
+		} catch (error) {
+			return next(error);
+		}
+	},
 ];
