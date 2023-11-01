badc0f5b1d55125d23b5742598f16e322cdfe406
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-01 05:38:04 +0100

    importing route

--- routes/signup.ts
+++ routes/signup.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup_get } from '../controllers/authContoller';
+import { signup_get, signup_post } from '../controllers/authContoller';
 
 const router = express.Router();
 
 router.get('/signup', signup_get);
+router.post('/signup', signup_post);
 
 export default router;
