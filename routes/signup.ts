1bcd7dbf025a6ca05f831e2009962ad9f53cea2f
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-02 13:27:32 +0100

    creation of login view

--- routes/signup.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { signup_get, signup_post } from '../controllers/authContoller';
-
-const router = express.Router();
-
-router.get('/signup', signup_get);
-router.post('/signup', signup_post);
-
-export default router;
