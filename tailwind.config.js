e3576dfc907281cd3d2b1c52db91ce6d87e4cdf9
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-05 16:46:52 +0100

    Add 'wild-sand' color palette to theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ['./views/*.hbs'],
 	theme: {
-		extend: {},
+		extend: {
+			'wild-sand': {
+				50: '#f3f4f6',
+				100: '#edeef1',
+				200: '#d7dbe0',
+				300: '#b3bbc6',
+				400: '#8a96a6',
+				500: '#6c798b',
+				600: '#566173',
+				700: '#474f5d',
+				800: '#3d444f',
+				900: '#363b44',
+				950: '#24272d',
+			},
+		},
 	},
+	variants: {},
 	plugins: [{ tailwindcss: {}, autoprefixer: {} }],
 };
